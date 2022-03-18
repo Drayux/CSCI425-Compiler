72da9f09f99a2d0bf3b85e83a569b951ea712879
= <vcubefinatic@gmail.com>
2022-03-18 08:23:08 -0600

    Create framework for luther

---
+++ parse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "src/list.h"
+#include "src/util.h"
+#include "src/dfa.h"
+#include "src/token.h"
+
+// LUTHER FUNCTIONS (for now)
+
+// TODO parse scan file
+
+// TODO create array of character lengths to line count (index)
+
+int main(int argc, char** argv) {
+	printf("Beginning parse!\n");
+	return 0;
+}
